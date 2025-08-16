1c7e2663b7429521eecc3e5c5b13a76772b7c28e
Jack Waterloo <jack.waterloo77@gmail.com>
2025-08-16 10:13:24 -0500

    added some button listeners

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import "./styles/main.css";
 // alert("Javascript connected")
 import homePage from "./components/home-page";
-import menuPage from "./components/menu-page"
+import menuPage from "./components/menu-page";
 import loadPage from "./load-page";
 
 loadPage(homePage);
+
+// attach btn listeners for page navigation
+// home btn 
+const homeBtn = document.querySelector(".homeBtn");
+homeBtn.addEventListener("click", (e) => {
+  loadPage(homePage);
+});
+// menu btn 
+const menuBtn = document.querySelector(".menuBtn");
+menuBtn.addEventListener("click", (e) => {
+  loadPage(menuPage);
+});
+// about btn 
